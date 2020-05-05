ac1b7853a6fd85e493e0b76c9920ae565dd326f0
573638328 <573638328@qq.com>
2020-05-05 15:21:10 +0800

    full pre-activation ResNet

--- models/resnet.py
+++ models/resnet.py
@@ -25,10 +25,10 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(x))
+        out = F.relu(self.norm1(x))
         shortcut = self.shortcut(out) if hasattr(self, 'shortcut') else x
         out = self.conv1(out)
-        out = self.conv2(F.relu(self.bn2(out)))
+        out = self.conv2(F.relu(self.norm2(out)))
         out += shortcut
         return out
 
