2cf26b1bf44daab3e7b019fced7c12e792919eda
1170300930 <1352951319@qq.com>
2020-05-05 19:31:04 +0800

    add lr ch

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     testdata = data.Testdata(opt)
     model = model.Model(opt)
     model.initialize(opt)
-    start = 0
+    start = 1
     if opt.continue_train:
         start = opt.loadepoch
     for epoch in range(start, opt.epoches):
@@ -21,5 +21,5 @@ if __name__ == "__main__":
             model.compute_visuals(epoch,traindata,testdata)
         if epoch % opt.save_freq == 0:
             model.save_networks(epoch)
-        if opt.lr_policy != 'none':
+        if opt.lr_policy != 'none' and epoch % opt.lr_decay_iters == 0:
             model.update_learning_rate()
